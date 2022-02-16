49443e51a48355e2d143ab618ec63ae445f14e40
Mica <97194550+greend25@users.noreply.github.com>
2022-02-16 14:23:31 -0300

    Add files via upload

---
+++ desafios/3 - ciclos e iteraciones/Ciclo.js
@@ -0,0 +1,15 @@
+// let numero = parseInt(prompt("ingrese numero"))
+// let suma = ""
+// for(let i = 0; i < 10; i++) {
+//     suma = (numero + 1);
+//     alert("su numero ahora es " + suma);
+// }
+
+
+let edad = parseInt(prompt("cuántos años tenés?"))
+
+if (edad >= 18) { alert("podés obtener tu carnet de conducir")
+    
+}
+else { alert("no estás habilitado para obtener tu carnet de conducir")}
+
