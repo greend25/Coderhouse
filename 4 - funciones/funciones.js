24db68b0c0faa3512a4544de2c6026c9976517f4
atareao <micka9727@hotmail.com>
2022-02-23 12:36:21 -0300

    tarea

--- 4 - funciones/funciones.js
+++ 4 - funciones/funciones.js
@@ -1,54 +1,6 @@
 
 
 
-// alert (`DOLCI VEGAN - Opciones disponibles 25 y 26 de febrero:
-// * Lemon Pie (8 porciones) $2100 
-// * Marquisse Franui (8 porciones) $3000`)
-
-// const lemon = 2100
-// const franui = 3000
-
-// const totalLemon = (a, b) => lemon * cantLemon;
-// const totalFranui = (a, b) => franui * cantFranui;
-// const total = (totalLemon, totalFranui) => totalLemon + totalFranui;
-
-
-
-// let cantFranui; 
-// let cantLemon;
-// let suma = 0
-
-// while (true) { 
-//     cantLemon = prompt("Elija la cantidad de unidades de Lemon Pie. Máximo 3");
-
-//     if (parseInt(cantLemon) < 0 || parseInt(cantLemon) > 3) {
-//         alert("Ingrese un número válido") }
-       
-//          else {
-//         console.log (cantLemon);
-         
-        
-//         cantFranui = prompt ("Elija la cantidad de unidades de Marquisse Franui. Máximo 3") 
-
-//           if (parseInt(cantFranui) < 0 || parseInt(cantFranui) > 3) {
-//              alert("Ingrese un número válido") }
-            
-//          else { 
-//          console.log(cantFranui);
-//          totalFranui(franui, cantFranui);
-//         console.log(totalFranui);
-//          }
-//          }
-    
-//     }
-        
-//      suma = parseInt(cantFranui) + parseInt(cantLemon)
-//      console.log(suma);
-
-// alert(`Su pedido es: ${cantLemon} Lemon Pie y ${cantFranui} Marquisse Franui 
-// En total $ ${total}`)
-
-
 
 
 alert (`DOLCI VEGAN - Opciones disponibles 25 y 26 de febrero:
