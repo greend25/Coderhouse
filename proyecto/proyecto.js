c34da6d1012bb124f53d4512b0f75f6f7af5f4fa
Mica <97194550+greend25@users.noreply.github.com>
2022-03-14 21:52:39 -0300

    Add files via upload

--- proyecto/proyecto.js
+++ proyecto/proyecto.js
@@ -1,10 +1,6 @@
 
 
 
-alert (`DOLCI VEGAN - Opciones disponibles 12 y 13 de marzo:
-* Lemon Pie (8 porciones) $2100 
-* Marquisse (8 porciones) $3000`)
-
 let productos = [];
 
 class Producto {
@@ -15,55 +11,154 @@ class Producto {
     }
 }
 
+const Vruce = new Producto ("Vruce", "torta", 3100);
+const Cabsha = new Producto ("Cabsha", "tarta", 2800);
+const Cumple = new Producto ("Torta de Cumpleaños", "torta", 2400);
+const Franui = new Producto ("Marquisse Franui", "torta", 3000);
 const Lemon = new Producto ("Lemon Pie", "tarta", 2100);
-const Marquisse = new Producto ("Marquisse", "torta", 3000);
+const Choco = new Producto ("Chocotorta", "torta", 2800);
 
-productos.push(Lemon, Marquisse)
+productos.push(Vruce, Cabsha, Cumple, Franui, Lemon, Choco)
 console.log(productos);
 
-function multi (a, b) {
-    return a * b}
-    
-    function sumar(a, b) {
-       return a + b 
-    }
+// const DOMitems = document.querySelector('.items');
+// const DOMcarrito = document.querySelector('#carrito');
+// const DOMtotal = document.querySelector('#total');
+// const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
-productos.forEach ( e => {
+function multi(a, b) {
+    return a * b;
+}
+    
+function sumar(a, b, c, d, e, f) {
+    return a + b; 
+}
+
+productos.forEach (e => {
     console.log("Precio pagando en efectivo " + precioConDescuento(e.precio));
 })
 
 function precioConDescuento(precio) {
-    return precio - (precio * 0.10)
+    return precio - (precio * 0.10);
 }
 
-let lemonEfectivo = precioConDescuento(Lemon.precio)
-let marquisseEfectivo = precioConDescuento(Marquisse.precio)
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
+let btn4 = document.getElementById("btn4")
+let btn5 = document.getElementById("btn5")
+let btn6 = document.getElementById("btn6")
 
-do {
-    let cantLemon = prompt("Ingrese cuántos Lemon Pie desea encargar - en números")
-    let cantMarquisse = prompt("Ingrese cuántas Marquisse desea encargar - en números")
-    
+let carrito = [];
+cantFr = 0;
+cantCab = 0;
+cantVr = 0;
+cantLe = 0;
+cantCh = 0;
+cantCum = 0;
 
-let totalLemon = multi(Lemon.precio, cantLemon)
-let totalMarquisse = multi(Marquisse.precio, cantMarquisse)
+btn1.addEventListener ("click", () => {
+    alert(`Añadiste ${Vruce.nombre} al carrito`)
+    const carritoDOM = document.getElementById("carrito")
+    const nuevoProducto = document.createElement("ul")
+    nuevoProducto.innerText = (`${Vruce.nombre} - $${Vruce.precio}`)
+    carritoDOM.appendChild(nuevoProducto)
 
-let total = sumar(totalLemon, totalMarquisse)
+    const cantVr = parseInt(Vruce.precio)
+    carrito.push(cantVr)
+})
 
-var pago = parseInt(prompt(`El total es de $${total}. Cómo va abonar? 1 - EFECTIVO / 2 - OTRO MEDIO DE PAGO`)) 
-if (pago == 2) {
-    alert("Su pedido ha sido confirmado")
+btn2.addEventListener ("click", () => {
+    alert(`Añadiste ${Cabsha.nombre} al carrito`)
+    const carritoDOM = document.getElementById("carrito")
+    const nuevoProducto = document.createElement("ul")
+    nuevoProducto.innerText = (`${Cabsha.nombre} - $${Cabsha.precio}`)
+    carritoDOM.appendChild(nuevoProducto)
+
+    const cantCab = parseInt(Cabsha.precio)
+    carrito.push(cantCab)
+})
+
+btn3.addEventListener ("click", () => {
+    alert(`Añadiste ${Cumple.nombre} al carrito`)
+    const carritoDOM = document.getElementById("carrito")
+    const nuevoProducto = document.createElement("ul")
+    nuevoProducto.innerText = (`${Cumple.nombre} - $${Cumple.precio}`)
+    carritoDOM.appendChild(nuevoProducto)
+
+    const cantCum = parseInt(Cumple.precio)
+    carrito.push(cantCum)
+})
+
+btn4.addEventListener ("click", () => {
+    alert(`Añadiste ${Franui.nombre} al carrito`)
+    const carritoDOM = document.getElementById("carrito")
+    const nuevoProducto = document.createElement("ul")
+    nuevoProducto.innerText = (`${Franui.nombre} - $${Franui.precio}`)
+    carritoDOM.appendChild(nuevoProducto)
+
+    const cantFr = parseInt(Franui.precio)
+    carrito.push(cantFr)
+})
+
+btn5.addEventListener ("click", () => {
+    alert(`Añadiste ${Lemon.nombre} al carrito`)
+    const carritoDOM = document.getElementById("carrito")
+    const nuevoProducto = document.createElement("ul")
+    nuevoProducto.innerText = (`${Lemon.nombre} - $${Lemon.precio}`)
+    carritoDOM.appendChild(nuevoProducto)
+
+    const cantLe = parseInt(Lemon.precio)
+    carrito.push(cantLe)
+})
+
+btn6.addEventListener ("click", () => {
+    alert(`Añadiste ${Choco.nombre} al carrito`)
+    const carritoDOM = document.getElementById("carrito")
+    const nuevoProducto = document.createElement("ul")
+    nuevoProducto.innerText = (`${Choco.nombre} - $${Choco.precio}`)
+    carritoDOM.appendChild(nuevoProducto)
+
+    const cantCh = parseInt(Choco.precio)
+    carrito.push(cantCh)
+})
+
+let sum = 0;
+for (let i = 0; i < carrito.length; i++) {
+    sum += carrito[i];
 }
-else {
-    let totalLemonE = multi(lemonEfectivo, cantLemon)
-    let totalMarquisseE = multi(marquisseEfectivo, cantMarquisse)
-    let totalEfectivo = sumar(totalLemonE, totalMarquisseE)
 
-    alert(`El total en efectivo es de $${totalEfectivo}. Su pedido ha sido confirmado`)
-}
+const totalDom = document.getElementById("total")
+const nuevoTotal = document.createElement("p")
+nuevoTotal.innerText = sum
+totalDom.appendChild(nuevoTotal)
 
-var terminar = prompt('1 - finalizar\n2 - empezar de nuevo');
+
+
+
+// do {
+//     let cantLemon = prompt("Ingrese cuántos Lemon Pie desea encargar - en números")
+//     let cantMarquisse = prompt("Ingrese cuántas Marquisse desea encargar - en números")
+
+//     let totalLemon = multi(Lemon.precio, cantLemon)
+//     let totalMarquisse = multi(Marquisse.precio, cantMarquisse)
+
+//     let total = sumar(totalLemon, totalMarquisse)
+
+//     var pago = parseInt(prompt(`El total es de $${total}. Cómo va abonar? 1 - EFECTIVO / 2 - OTRO MEDIO DE PAGO`)) 
+//         if (pago == 2) {
+//         alert("Su pedido ha sido confirmado")
+//             } else {
+//              let totalLemonE = multi(lemonEfectivo, cantLemon)
+//              let totalMarquisseE = multi(marquisseEfectivo, cantMarquisse)
+//              let totalEfectivo = sumar(totalLemonE, totalMarquisseE)
+
+//              alert(`El total en efectivo es de $${totalEfectivo}. Su pedido ha sido confirmado`)
+//             }
+
+//     var terminar = prompt('1 - finalizar\n2 - empezar de nuevo');
   
-} while (terminar == 2);
+// } while (terminar == 2);
 
 
 
